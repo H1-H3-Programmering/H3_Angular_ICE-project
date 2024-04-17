b3f739dc656ced58a62df9578039023b010a072f
MRasch1 <mrasch1102@gmail.com>
2024-04-17 13:33:59 +0200

    New angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test-project';
+  // variable name : Type = value
+  title = 'test-project'; //javascript
+  string = 'samuraiProject02';
+  Name: string = 'Jens'; //typescript
+  Age: number = 20;
+  Sand: boolean = true;
+  Two_Way_test: string ='';
+
+  list: string[] = ['one','two','three'];
+
+  PersonList: string[] =['Jens','Hans','Morten','Karsten'];
+
+  ngOnInit(): void{
+    console.log("first test");
+    console.log(this.list);
+
+    this.create(); // this is a "pointer" to the object
+
+  }
+  //function
+create(): void{
+  console.log("Create function called");
+}
+
+
 }
