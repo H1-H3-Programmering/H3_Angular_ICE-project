7de672b435f23978800e4e6d851b876cf7ffdb82
MRasch1 <mrasch1102@gmail.com>
2024-04-30 09:45:26 +0200

    work-in-progress: comments crud

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Comments } from '../../models/Comments';
 import { ICEServiceService } from '../../services/ice-service.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { text } from 'stream/consumers';
 
 @Component({
   selector: 'app-comments',
@@ -9,6 +11,16 @@ import { ICEServiceService } from '../../services/ice-service.service';
 })
 export class CommentsComponent {
   commentsList: Comments[] = [];
+  commentsForm: FormGroup;
+  selectedEntityId: number | null = null; // Property to store the selected entity ID
+  showCommentsBtn: boolean = false;
+
+  constructor(private service: ICEServiceService<Comments>) {
+    this.commentsForm = new FormGroup({
+      recipeId: new FormControl(null,[Validators.required]),
+      text: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    });
+  }
 
   ngOnInit(): void {
     this.service.getAllComments().subscribe((data) => {
@@ -17,5 +29,4 @@ export class CommentsComponent {
     });
   }
 
-  constructor(private service: ICEServiceService<Comments>) {}
 }
